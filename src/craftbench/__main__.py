c29c93e8675783cce1bb698873861b312895e5ed
Mathïs Fédérico <mathfederico@gmail.com>
2023-02-23 21:23:25 +0100

    🔥 Remove main content

--- src/craftbench/__main__.py
+++ src/craftbench/__main__.py
@@ -1,6 +1,2 @@
-from craftbench.maskable_ppo import benchmark_mskppo
-
-
 def main():
-    print(f"{'CraftingEnvBenchmark': -^40}")
-    benchmark_mskppo(save_req_graph=True, save_sol_graph=True)
+    pass
