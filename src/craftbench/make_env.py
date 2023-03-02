ebc1ad790f63ccd663123c77b3701b826567605a
Mathïs Fédérico <mathfederico@gmail.com>
2023-03-02 14:09:48 +0100

    ⚰️ Remove dead code

--- src/craftbench/make_env.py
+++ src/craftbench/make_env.py
@@ -1,25 +1,14 @@
-from typing import Optional, Union
-import os
-
 import numpy as np
-import gym
 
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder
 
 from crafting import CraftingEnv
-from crafting.examples import (
-    MineCraftingEnv, RandomCraftingEnv,
-    TowerCraftingEnv,
-    RecursiveCraftingEnv,
-    LightRecursiveCraftingEnv
-)
-
 
 
 def record_wrap_env(crafting_env: CraftingEnv, video_path: str):
-    crafting_env = Monitor(crafting_env)  # record stats such as returns
-    env = DummyVecEnv([lambda: crafting_env])
+    env = Monitor(crafting_env)  # record stats such as returns
+    env = DummyVecEnv([lambda: env])
     env = VecVideoRecorder(
         env,
         video_path,
